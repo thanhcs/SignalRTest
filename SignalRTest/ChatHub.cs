456cf577c7f003048b8f98ad89d5255492fef339
Thanh Nguyen <thanhnguyencs@utexas.edu>
2017-03-13 23:04:07 -0500

    Initial commit - Use to test SignalR in GMX solution

---
+++ SignalRTest/ChatHub.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace SignalRTest
+{
+    public class ChatHub : Hub
+    {
+        public void send(string name, string message)
+        {
+            Clients.All.broadcastMessage(new Mail()
+            {
+                Sender = name,
+                Subject = message
+            });
+        }
+    }
+
+    public class Mail
+    {
+        public string Sender { get; set; }
+        public string Subject { get; set; }
+
+    }
+}
